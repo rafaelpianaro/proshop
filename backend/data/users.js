5a0f9766713a91a69e9c1da7c4cf7321b56e0d26
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-23 17:06:04 -0300

    added for field password bcryptjs

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,18 +1,22 @@
-const user = [
+import bcrypt from 'bcryptjs'
+
+const users = [
 	{
 		name: 'Admin User',
 		email: 'admin@admin.com',
-		password: xxx,
+		password: bcrypt.hashSync('123456', 10),
 		isAdmin: true
 	},
 	{
 		name: 'João Silva',
 		email: 'joao@joao.com',
-		password: xxx
+		password: bcrypt.hashSync('123456', 10),
 	},
 	{
 		name: 'Maria Silva',
 		email: 'maria@maria.com',
-		password: xxx
+		password: bcrypt.hashSync('123456', 10),
 	},
 ]
+
+export default users
