1a1c0b02d6c6f63f43b023d4e94b658c94fcce91
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-23 17:00:26 -0300

    start seed

---
+++ backend/data/users.js
@@ -0,0 +1,18 @@
+const user = [
+	{
+		name: 'Admin User',
+		email: 'admin@admin.com',
+		password: xxx,
+		isAdmin: true
+	},
+	{
+		name: 'João Silva',
+		email: 'joao@joao.com',
+		password: xxx
+	},
+	{
+		name: 'Maria Silva',
+		email: 'maria@maria.com',
+		password: xxx
+	},
+]
