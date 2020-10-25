52a1146b95427e6888f7bee7d51c39d9944404d7
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-24 23:39:43 -0300

    added some validations for each statusCode

--- backend/index.mjs
+++ backend/index.mjs
@@ -11,11 +11,31 @@ connectDB()
 
 const app = express()
 
+// app.use((req, res, next) => {
+//     console.log('req', req.originalUrl)
+//     next()
+// })
+
 app.get('/', (req, res) => {
     res.send('API is running')
 })
 
 app.use('/api/products', productRoutes)
+
+app.use((req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`)
+    res.status(404)
+    next(error)
+})
+
+app.use((err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    res.status(statusCode)
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+    })
+})
 
 const PORT = process.env.PORT || 5000
 
