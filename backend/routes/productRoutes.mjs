e17617e28d11974aae20056239c51d0a7d6c786f
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-24 23:15:52 -0300

    added asyncHandler for valid the requests

--- backend/routes/productRoutes.mjs
+++ backend/routes/productRoutes.mjs
@@ -1,16 +1,29 @@
 import express from 'express'
+import asyncHandler from 'express-async-handler'
 import Product from '../models/productModel.mjs'
 
 const router = express.Router()
 
-router.get('/', async(req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-})
+// @desc Fetch all products
+// @route GET /api/products
+// @access Public
+router.get('/', asyncHandler(async(req, res) => {
+	const products = await Product.find({})
+	res.json(products)
+}))
 
-router.get('/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
+// @desc Fetch single product
+// @route GET /api/products/:id
+// @access Public
+router.get('/:id', asyncHandler(async(req, res) => {
+	// console.log('req', req.params.id)
+	const product = await Product.findById(req.params.id)
+	// console.log('product', product)
+	if(product){
+		res.json(product)
+	}else{
+		res.status(404).json({ message: 'Product not found' })
+	}
+}))
 
 export default router
