fa70663a07617fb49fea874bb07935496ee0a809
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-11-10 23:36:48 -0300

    custom authentication middleware

--- backend/routes/userRoutes.mjs
+++ backend/routes/userRoutes.mjs
@@ -1,8 +1,10 @@
 import express from 'express'
-import {authUser} from '../controllers/userController.mjs'
+import {authUser, getUserProfile} from '../controllers/userController.mjs'
+import {protect} from '../middleware/authMiddleware.mjs'
 
 const router = express.Router()
 
 router.post('/login', authUser) 
+router.route('/profile').get(protect, getUserProfile)
 
 export default router
