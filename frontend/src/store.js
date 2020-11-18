b5cd3c8020f7205ae710080cd564b8ef7a2a01a5
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-11-17 22:09:27 -0300

    added user register, reducer, action and screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,13 +3,14 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from  './reducers/cartReducer'
-import { userLoginReducer } from  './reducers/usersReducers'
+import { userLoginReducer, userRegisterReducer } from  './reducers/usersReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
-    userLogin: userLoginReducer
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
