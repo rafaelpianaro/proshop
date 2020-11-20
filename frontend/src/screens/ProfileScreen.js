faba95e78ee292ffe1634806abc967f8bc1b99b5
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-11-20 19:16:27 -0300

    update user profile

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,10 +1,10 @@
 import React, {useState, useEffect} from 'react'
-import {Link} from 'react-router-dom'
 import {Form,Button,Row,Col} from 'react-bootstrap'
 import {useDispatch, useSelector} from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import {getUserDetails} from '../actions/userActions'
+import {getUserDetails, updateUserProfile} from '../actions/userActions'
+import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants'
 
 const ProfileScreen = ({location, history}) => {
     const [name, setName] = useState('')
@@ -21,18 +21,22 @@ const ProfileScreen = ({location, history}) => {
     const userLogin = useSelector(state => state.userLogin)
     const {userInfo} = userLogin
 
+    const userUpdateProfile = useSelector(state => state.userUpdateProfile)
+    const {success} = userUpdateProfile
+
     useEffect(() => {
         if(!userInfo){
-            history.push('/')
+            history.push('/login')
         } else {
-            if(!user.name){
+            if(!user || !user.name || success){
+                dispatch({type: USER_UPDATE_PROFILE_RESET})
                 dispatch(getUserDetails('profile'))
             } else {
                 setName(user.name)
                 setEmail(user.email)
             }
         }
-    },[dispatch, history, userInfo, user])
+    },[dispatch, history, userInfo, user, success])
 
     const submitHandler = (e) => {
         e.preventDefault()
@@ -41,6 +45,7 @@ const ProfileScreen = ({location, history}) => {
             setMessage('Passwords do not match')
         } else {
             // DISPATCH UPDATE PROFILE
+            dispatch(updateUserProfile({ id: user._id, name, email, password }))
         }
 
     }
@@ -50,6 +55,7 @@ const ProfileScreen = ({location, history}) => {
                 <h2>User Profile</h2>
                 {message && <Message variant='danger'>{message}</Message>}
                 {error && <Message variant='danger'>{error}</Message>}
+                {success && <Message variant='success'>Profile Updated</Message>}
                 {loading && <Loader />}
                 <Form onSubmit={submitHandler}>
                     <Form.Group controlId='name'>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from  './reducers/cartReducer'
-import { userLoginReducer, userRegisterReducer, userDetailsReducer } from  './reducers/usersReducers'
+import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from  './reducers/usersReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -11,7 +11,8 @@ const reducer = combineReducers({
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
-    userDetails: userDetailsReducer
+    userDetails: userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
