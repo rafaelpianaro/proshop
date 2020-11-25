d0eec287f8c11869e7e82056e762630fe929d0b4
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-11-25 19:24:40 -0300

    created get order by ID endpoint - backend

--- backend/routes/orderRoutes.mjs
+++ backend/routes/orderRoutes.mjs
@@ -1,9 +1,10 @@
 import express from 'express'
-import {addOrderItems} from '../controllers/orderController.mjs'
+import {addOrderItems, getOrderById} from '../controllers/orderController.mjs'
 import {protect} from '../middleware/authMiddleware.mjs'
 
 const router = express.Router()
 
 router.route('/').post(protect, addOrderItems)
+router.route('/:id').get(protect, getOrderById)
 
 export default router
