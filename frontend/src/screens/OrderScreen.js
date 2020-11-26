ba6517b29e09a6fae303fabaa378a5a04b23c09a
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-11-26 20:24:40 -0300

    added paypal payments

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,11 +1,13 @@
 import React, {useState,useEffect} from 'react'
 import axios from 'axios'
+import {PayPalButton} from 'react-paypal-button-v2'
 import {Link} from 'react-router-dom'
-import {Row,Col,ListGroup,Image} from 'react-bootstrap'
+import {Row,Col,ListGroup,Image,Card} from 'react-bootstrap'
 import {useDispatch, useSelector} from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import {getOrderDetails} from '../actions/orderActions'
+import {getOrderDetails, payOrder} from '../actions/orderActions'
+import { ORDER_PAY_RESET } from '../constants/orderConstants'
 
 const OrderScreen = ({match}) => {
     const orderId = match.params.id
@@ -34,7 +36,7 @@ const OrderScreen = ({match}) => {
             const { data: clientId } = await axios.get('/api/config/paypal')
             const script = document.createElement('script')
             script.type = 'text/javascript'
-            script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`
+            script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}&currency=BRL`
             script.async = true
             script.onload = () => {
                 setSdkReady(true)
@@ -42,6 +44,7 @@ const OrderScreen = ({match}) => {
             document.body.appendChild(script)
         }
         if(!order || successPay){
+            dispatch({ type: ORDER_PAY_RESET })
             dispatch(getOrderDetails(orderId))
         }else if(!order.isPaid){
             if(!window.paypal){
@@ -56,6 +59,11 @@ const OrderScreen = ({match}) => {
         // }
     // }, [order, orderId])
     }, [dispatch, orderId, successPay, order])
+
+    const successPaymentHandler = (paymentResult) => {
+        console.log(paymentResult)
+        dispatch(payOrder(orderId, paymentResult))
+      }
 
     return loading ? <Loader /> : error ? <Message variant='danger'>{error}</
     Message> : <>
@@ -108,42 +116,55 @@ const OrderScreen = ({match}) => {
                 </ListGroup>
              </Col>
              <Col md={4}>
-                 <ListGroup variant='flush'>
-                     <ListGroup.Item>
-                         <h2>Order Summary</h2>
-                     </ListGroup.Item>
-                     <ListGroup.Item>
-                         <Row>
-                             <Col>Items</Col>
-                             <Col>${order.itemsPrice}</Col>
-                         </Row>
-                     </ListGroup.Item>
-                     <ListGroup.Item>
-                         <Row>
-                             <Col>Shipping</Col>
-                             <Col>${order.shippingPrice}</Col>
-                         </Row>
-                     </ListGroup.Item>
-                     <ListGroup.Item>
-                         <Row>
-                             <Col>Tax</Col>
-                             <Col>${order.taxPrice}</Col>
-                         </Row>
-                     </ListGroup.Item>
-                     <ListGroup.Item>
-                         <Row>
-                             <Col>Total</Col>
-                             <Col>${order.totalPrice}</Col>
-                         </Row>
-                     </ListGroup.Item>
-                     <ListGroup.Item>
-                        {error && <Message variant='danger'>{error}</Message>}
-                     </ListGroup.Item>
-
-                     {/* <ListGroup.Item>
-                        <Button type='button' className='btn-block' disabled={order.orderItems === 0} onClick={placeOrderHandler}>Place Order</Button>
-                    </ListGroup.Item> */}
-                 </ListGroup>
+                <Card>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h2>Order Summary</h2>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>Items</Col>
+                                <Col>${order.itemsPrice}</Col>
+                            </Row>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>Shipping</Col>
+                                <Col>${order.shippingPrice}</Col>
+                            </Row>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>Tax</Col>
+                                <Col>${order.taxPrice}</Col>
+                            </Row>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>Total</Col>
+                                <Col>R${order.totalPrice}</Col>
+                            </Row>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            {error && <Message variant='danger'>{error}</Message>}
+                        </ListGroup.Item>
+                        {!order.isPaid && (
+                            <ListGroup.Item>
+                            {loadingPay && <Loader />}
+                            {!sdkReady ? (
+                                <Loader />
+                            ) : (
+                                <PayPalButton
+                                amount={order.totalPrice}
+                                currency='BRL'
+                                onSuccess={successPaymentHandler}
+                                />
+                            )}
+                            </ListGroup.Item>
+                        )}
+                        
+                    </ListGroup>
+                 </Card>
              </Col>
         </Row>
     </>
