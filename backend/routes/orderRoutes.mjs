43b9d3cc2317b892b5b1c5e764a07acdda686d9e
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-11-29 22:01:57 -0300

    added show orders on profile

--- backend/routes/orderRoutes.mjs
+++ backend/routes/orderRoutes.mjs
@@ -1,10 +1,11 @@
 import express from 'express'
-import {addOrderItems, getOrderById, updateOrderToPaid} from '../controllers/orderController.mjs'
+import {addOrderItems, getOrderById, updateOrderToPaid, getMyOrders} from '../controllers/orderController.mjs'
 import {protect} from '../middleware/authMiddleware.mjs'
 
 const router = express.Router()
 
 router.route('/').post(protect, addOrderItems)
+router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
 
