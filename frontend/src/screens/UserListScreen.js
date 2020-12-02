7c8aa0f98ab57b1626bb6716705805728b39fa08
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-12-02 19:09:49 -0300

    added admin user delete on UserListScreen

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -4,7 +4,7 @@ import {Table,Button} from 'react-bootstrap'
 import {useDispatch, useSelector} from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import {listUsers} from '../actions/userActions'
+import {listUsers, deleteUser} from '../actions/userActions'
 
 const UserListScreen = ({ history }) => {
     const dispatch = useDispatch()
@@ -16,8 +16,8 @@ const UserListScreen = ({ history }) => {
     const userLogin = useSelector((state) => state.userLogin)
     const { userInfo } = userLogin
 
-    // const userDelete = useSelector((state) => state.userDelete)
-    // const { success: successDelete } = userDelete
+    const userDelete = useSelector((state) => state.userDelete)
+    const { success: successDelete } = userDelete
 
     useEffect(() => {
         if (userInfo && userInfo.isAdmin) {
@@ -25,13 +25,13 @@ const UserListScreen = ({ history }) => {
         } else {
           history.push('/login')
         }
-      }, [dispatch, history, userInfo])
+      }, [dispatch, history, successDelete, userInfo])
 
     const deleteHandler = (id) => {
         console.log('delete')
-        // if (window.confirm('Are you sure')) {
-        //   dispatch(deleteUser(id))
-        // }
+        if (window.confirm('Are you sure')) {
+          dispatch(deleteUser(id))
+        }
       }
 
     return (
