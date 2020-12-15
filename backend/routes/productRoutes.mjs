108d5207b954cae826561db44a6d9ce684f9f3ce
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-12-14 22:22:03 -0300

    created admin delete products

--- backend/routes/productRoutes.mjs
+++ backend/routes/productRoutes.mjs
@@ -1,9 +1,10 @@
 import express from 'express'
-import {getProducts, getProductById} from '../controllers/productController.mjs'
+import {getProducts, getProductById, deleteProduct} from '../controllers/productController.mjs'
+import {protect,admin} from '../middleware/authMiddleware.mjs'
 
 const router = express.Router()
 
 router.route('/').get(getProducts)
-router.route('/:id').get(getProductById)
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
 
 export default router
